c7e7a5a1b9109042d933faf57c4d6f56685da3f0
ngineus <ngineus@gmail.com>
2017-04-12 00:43:32 +0300

    Folder Initialisation

---
+++ dist/js/script.js
@@ -0,0 +1,35 @@
+﻿$(function() { // jQuery Запускаем по окончании загрузки страницы
+  // $('carousel-list').carouselka();
+
+  $.fn.carouselka = function() { // К стандартному функционалу джиквери добавляем свой функционал
+    
+  }
+});
+
+(function($) { // jQuery плагин
+
+
+  return this;
+})(jQuery);
+
+
+
+
+CSS. темный фон для модального окна
+
+.modal_overlay {
+	position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+
+1. jQuery.fn
+2. Чтобы можно было реализовать чейнинг, последовательный вызов различных функций
+3. При помощи объекта, передаваемого в jQuery функцию
+4. Передать jQuery в замыкание: (function($){...})(jQuery)
+5. 
+
